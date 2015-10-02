ba1e53457ba9cd15e7bb0dd4a97ea9167370b5f5
Stephan Göppentin <dedda102@gmail.com>
2015-10-02 20:25:26 +0200

    added project

---
+++ src/wake.php
@@ -0,0 +1,13 @@
+<?php
+
+$macs = array(
+    "optimus-prime" => "D4:3D:7E:E3:4B:50",
+    "hans-dampf" => "BC:5F:F4:5B:76:CB"
+);
+
+$host = $_GET["host"];
+$mac = $macs[$host];
+
+exec("sudo etherwake $mac");
+
+echo '<html><head><meta http-equiv="refresh" content="0; URL=http:../"></head></html>';
