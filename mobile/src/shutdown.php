5cf5f645f2fc7c8b8ca33b7541e5383ebfc4a2e3
Stephan Göppentin <dedda102@gmail.com>
2015-10-25 10:48:00 +0100

    fix

--- mobile/src/shutdown.php
+++ mobile/src/shutdown.php
@@ -2,6 +2,6 @@
 
 $host = $_GET["host"];
 
-exec("ssh bmo@$host -t 'sudo shutdown -h now'");
+exec("ssh bmo@$host -T 'sudo shutdown -h now'");
 
 echo '<html><head><meta http-equiv="refresh" content="0; URL=http:../"></head></html>';
