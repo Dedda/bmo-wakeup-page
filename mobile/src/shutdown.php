2bd366300d3f2ba8237cd157c57f456d463f06c8
Stephan Göppentin <dedda102@gmail.com>
2015-10-25 10:46:58 +0100

    merged mobile pages

--- mobile/src/shutdown.php
+++ mobile/src/shutdown.php
@@ -4,4 +4,4 @@ $host = $_GET["host"];
 
 exec("ssh bmo@$host -t 'sudo shutdown -h now'");
 
-echo '<html><head><meta http-equiv="refresh" content="0; URL=http:../shutup.html"></head></html>';
+echo '<html><head><meta http-equiv="refresh" content="0; URL=http:../"></head></html>';
